68342fee6416595fd4e3d06a98523ade9ff658be
Aondofa James <terkimbijames@gmail.com>
2023-07-18 19:44:40 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main :- prints out a string
+ * return: 0 success
+ */
+int main(void)
+{
+
+	char c = _putchar;
+
+	printf("%s/n" c);
+	return (0);
+}
